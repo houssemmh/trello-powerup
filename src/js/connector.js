4eaf4bc6b91bd4afc48e9b664ba11b5121a88f17
Houssem Daoud <hdaoud@thecloudconnectors.com>
2024-04-23 13:43:43 -0400

    test buttons

--- src/js/connector.js
+++ src/js/connector.js
@@ -112,6 +112,12 @@ window.TrelloPowerUp.initialize(
         height: 281,
       });
     },
+    'card-buttons': function(t, options){
+      return [{
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Houssem is rich',
+      }];
+    },
   },
   {
     localization: localizationSettings,
