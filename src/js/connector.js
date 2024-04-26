a876a14e3e77a806b94057cf839a3656e438c966
Houssem Daoud <hdaoud@thecloudconnectors.com>
2024-04-26 09:30:14 -0400

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -106,11 +106,23 @@ console.log("money")
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, options) {
     return t.card('all').then(function(card) {
-     
+      return t.get(card.id, 'shared', 'previousDates', { start: null, due: null } )
+        .then(function(previousDates) {
           if (card.start || card.due) {
-              console.log(`"${card.id}" "${card.name}": Current dates ${card.start} - ${card.due}`);
+              console.log(`Initial date - "${card.id}" "${card.name}": Current dates ${card.start} - ${card.due}`);
           }
+
+          const startChanged = card.start !== previousDates.start;
+          const dueChanged = card.due !== previousDates.due;
+
+          if (startChanged || dueChanged) {
+            console.log(`date change - "${card.id}" "${card.name}": Date changed from ${previousDates.start} - ${previousDates.due} to ${card.start} - ${card.due}`);
+          }
+          
+          t.set(card.id, 'shared', 'previousDates', { start: card.start, due: card.due });
+
           return [];
+        });
     });
   }
 }, {
